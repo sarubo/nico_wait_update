e74d2a89f2493d0fae70d488e7a0c4087e6d9bfb
sarubo <sarubo2016@gmail.com>
2024-06-04 02:31:57 +0900

    広告非表示の方法をニコニコに優しく

--- script/remove_ad.js
+++ script/remove_ad.js
@@ -3,16 +3,12 @@ console.log = () => { }
 console.log("script/remove_ad.js");
 
 /**
- * @param {string[]} classNames
- * @param {string[]} idNames
+ * @param {string[]} selectors
  */
-const addClass = (classNames, idNames) => {
-  classNames.flatMap((className) =>
-    Array.from(document.getElementsByClassName(className))
+const addClass = (selectors) => {
+  selectors.flatMap((selector) =>
+    Array.from(document.querySelectorAll(selector))
   )
-    .concat(idNames
-      .map((id) => document.getElementById(id))
-      .filter((v) => v))
     .forEach((elm) => {
       console.log(elm);
       elm.classList.add("disabled-ad");
@@ -21,21 +17,20 @@ const addClass = (classNames, idNames) => {
 }
 
 /**
- * @param {string[]} classNames
- * @param {string[]} idNames
+ * @param {string[]} selectors
  */
-const remove = (classNames, idNames) => {
+const remove = (selectors) => {
   document.addEventListener("keyup", (en) => {
     console.log("keyup");
     if (en.key !== "Escape") {
       return;
     }
     console.log("Escape");
-    addClass(classNames, idNames);
+    addClass(selectors);
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("load");
-  remove(["Ads", "ads", "nicoadVideoItem", "RectangleAdContainer", "BottomSideContainer-stickyArea", "BottomContainer-adContainer"], ["LEFT_AD_ELEMENT", "RIGHT_AD_ELEMENT", "dic_pc_300x250_east"]);
+  remove([".Ads", ".ads", ".nicoadVideoItem", ".RectangleAdContainer", ".BottomSideContainer-stickyArea", ".BottomContainer-adContainer", "#LEFT_AD_ELEMENT", "#RIGHT_AD_ELEMENT", "#dic_pc_300x250_east", "#program-information-ad", "#ad-footer", ".___site-header___Vbvc8 > aside > div:nth-child(1)"]);
 });
